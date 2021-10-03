9fa0c5df934d23889c5310d89962abb3c186df1b
Severin Bretscher <sir_even@icloud.com>
2021-10-03 16:26:09 +0200

    Remove unused code

--- hummingbot/strategy/technical_analysis/start.py
+++ hummingbot/strategy/technical_analysis/start.py
@@ -11,7 +11,7 @@ from hummingbot.connector.exchange_base import ExchangeBase
 from decimal import Decimal
 
 from .ta.ta import TA
-from .ta.pattern_detection import PatternDetection
+# from .ta.pattern_detection import PatternDetection
 
 def start(self):
     try:
